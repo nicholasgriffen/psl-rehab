dd342efde9cc625f62ae9b80740308dd9fe3772e
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-09-11 18:53:07 -0600

    use path to gert a fulll path to the public folder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname"public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen((process.env.PORT || 3000), () => console.log("listening on 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("listening on 3000"));
