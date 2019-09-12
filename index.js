19abcb22493f4220f7e47507bd0949dec98affb8
nicholasgriffen <nicholas.s.griffen@gmail.com>
2019-09-11 18:51:47 -0600

    bind port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname"public"));
 
-app.listen(3000, () => console.log("listening on 3000"));
+app.listen((process.env.PORT || 3000), () => console.log("listening on 3000"));
